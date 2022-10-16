0085bf6976ddc4b07392a36e34a94787f1a74645
Tarun <113331216+t4runs@users.noreply.github.com>
2022-10-16 21:11:09 +0530

    Update honepy_pot.py

--- honey_pot.py
+++ honey_pot.py
@@ -1,20 +1,24 @@
 import socket
 from pyfiglet import figlet_format
 
-
+#for viewing the banner.
 ascii_banner = figlet_format('HoneyPot',font='ntgreek')
 print('\33[33m'+ascii_banner+'\33[0m')
 
+#intializing the socket.
 try:
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	print("Socket Successfully Created!!!")
+#if socket is not successfully created.
 except socket.error as s:
 	print("An error has occured while creating the socket.")
 
+#asking for the port number.
 port_number = int(input("Port Number to monitor: "))
+#listening on the local ip address and the port number.
 s.bind(('',port_number))
 no_connections = int(input("Listening Connections for: "))
-
+#Starts listening for the connections.
 s.listen(no_connections)
 
 while True:
