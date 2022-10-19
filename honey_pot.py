75ef978770c22731e545c98c4576a7e2f97d102d
Tarun <113331216+Cyb3rTarun@users.noreply.github.com>
2022-10-19 20:57:45 +0530

    Update honey_pot.py

--- honey_pot.py
+++ honey_pot.py
@@ -1,5 +1,45 @@
 import socket
 from pyfiglet import figlet_format
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+from_address = "linux.tarun1@gmail.com"
+to_address = "starun.py@gmail.com"
+
+msg = MIMEMultipart()
+
+msg['FROM'] = from_address
+
+msg['TO'] = to_address
+
+msg['Subject'] = "About Activity Login"
+
+body = "Here is the Intruders list with their Ip Address."
+
+msg.attach(MIMEText(body,'plain'))
+
+file_name = 'Intruders.txt'
+attachment = open(file_name, 'rb')
+
+p = MIMEBase('application', 'octet-stream')
+
+p.set_payload((attachment).read())
+
+encoders.encode_base64(p)
+
+p.add_header('Content-Disposition',f"attachment; filename = {file_name}")
+
+msg.attach(p)
+
+s = smtplib.SMTP('smtp.gmail.com',587)
+s.starttls()
+s.login(from_address, 'mizmtbbzdtmgeyds')
+text = msg.as_string()
+s.sendmail(from_address, to_address, text)
+s.quit()
 
 #for viewing the banner.
 ascii_banner = figlet_format('HoneyPot',font='ntgreek')
@@ -21,8 +61,14 @@ no_connections = int(input("Listening Connections for: "))
 #Starts listening for the connections.
 s.listen(no_connections)
 
+file = open('Intruders.txt','a')
+
 while True:
 	conn,addr = s.accept()
 	print(f"{addr} has been logged an activity.")
+	file.write(f'{addr} is trying to connect\n')	
 	conn.send(b'Your activity has been logged.')
 	conn.close()
+
+
+
